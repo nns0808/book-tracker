18d4f2adb803452edc023bfe942c56ee2bfc24b0
Natalia <nns0808@gmail.com>
2026-04-12 12:49:22 -0700

    fix backend API URL for production

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,3 @@
 // export const API_URL = "http://localhost:5000";
-export const API_URL = "https://book-tracker-server.onrender.com";
+// export const API_URL = "https://book-tracker-server.onrender.com";
+export const API_URL = "https://book-tracker-api-kc7b.onrender.com";
